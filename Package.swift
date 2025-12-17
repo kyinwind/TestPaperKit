fdde339e0b9fc81f33ec1be85f45cf1d18746d39
kylinwind <xuehui.yang888@icloud.com>
2025-12-18 00:50:20 +0800

    修改源码目录名称，和包名一致

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     targets: [
         .target(
             name: "TestPaperKit",
-            path: "Sources/TestPaper"
+            path: "Sources/TestPaperKit"
         )
     ]
 )
